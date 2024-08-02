f87a84e842721ff542f3a41db9e2c29f1da36197
Althaf2102 <fakhriza_faraby_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 14:58:41 +0700

    Add files via upload

---
+++ Tambah.java
@@ -0,0 +1,10 @@
+public class Tambah {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 200;
+        int hasil = a+b;  
+        System.out.println("Hasil Dari"+ a + "ditambah"+ b +"Adalah"+ hasil);
+
+    }
+    
+}
