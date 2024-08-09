fda0be73eaa8b4c5b72444480dd0dcd214b8f041
Althaf2102 <fakhriza_faraby_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 15:13:20 +0700

    Add files via upload

---
+++ Buku.java
@@ -0,0 +1,23 @@
+public class Buku {
+   int id;
+   String judul;
+   String pengarang;
+   int tahunterbit;
+
+   //constructor
+   //method yg bernama sama dgn nama class
+   public Buku(int id,String judul,String pengarang,int tahunterbit){
+    //variabel = parameter
+    this.id = id;
+    this.judul = judul;
+    this.pengarang = pengarang;
+    this.tahunterbit = tahunterbit;
+
+   }
+
+
+
+        
+    }
+    
+
---
+++ BukuTester.java
@@ -0,0 +1,11 @@
+public class BukuTester {
+    public static void main(String[] args) {
+        //object
+        Buku anak = new Buku(2102,"Malin Kundang","Althaf",2000);
+        Buku novel = new Buku(2121,"Si Putih","Tere Liye",2012);
+        Buku komik = new Buku(1616,"next G","muffin",2008);
+        Buku fantasi = new Buku(1613,"Harry Potter","JK Rowling",2003);
+        Buku sejarah = new Buku(2116,"Uni Soviet","althaf",2023);
+    }
+    
+}
