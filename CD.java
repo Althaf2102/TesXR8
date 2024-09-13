f64058c554f9f6ff7ca7147aa0082847738f617b
Althaf2102 <fakhriza_faraby_33rpl@student.smktelkom-mlg.sch.id>
2024-09-13 12:48:36 +0700

    Add files via upload

--- CD.java
+++ CD.java
@@ -1,7 +1,31 @@
 public class CD extends Product {
-    String artist;
-    int numSong;
-    String label;
+   protected String artist;
+  protected int numSong;
+  protected String label;
+
+  public String getArtist() {
+    return this.artist;
+  }
+
+  public void setArtist(String artist) {
+    this.artist = artist;
+  }
+
+  public int getNumSong() {
+    return this.numSong;
+  }
+
+  public void setNumSong(int numSong) {
+    this.numSong = numSong;
+  }
+
+  public String getLabel() {
+    return this.label;
+  }
+
+  public void setLabel(String label) {
+    this.label = label;
+  }
   
     public CD(){
       super();//mengakses constructor superclass
